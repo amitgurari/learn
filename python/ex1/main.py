52e3a80eee5b7018678f4182161825d8fadd83cf
amitgurari <amitgurari12@gmail.com>
2024-10-23 16:58:53 +0000

    dev

--- python/ex1/main.py
+++ python/ex1/main.py
@@ -1,2 +1,61 @@
 
 print("hello world")
+
+value = 17 * (8 + 15) - 2 / 10
+print(value)
+
+value = -2 + -2
+print(value)
+value = 390.28 + 150.57
+print(value)
+
+print(1+2)
+print(2-8)
+print(5*5)
+print(25/5)
+print(5**3)
+
+width = 7
+height = 10
+area = width * height
+print("area:", area)
+
+name = 'amit'
+print(name)
+name = "amit"
+print(name)
+
+noon = "12 o'clock"
+print(noon)
+noon = '12 o\'clock'
+print(noon)
+
+# first line
+# second line
+# third line
+multi_line = 'first line\nsecond line\nthird line'
+print(multi_line)
+multi_line = "first line\nsecond line\nthird line"
+print(multi_line)
+multi_line = 'first line\n\tsecond line\n\t\tthird line'
+print(multi_line)
+multi_line = """first line
+second line
+third line"""
+print(multi_line)
+
+print("1 hello", name)
+hello = "2 hello amit"
+print(hello)
+hello = "3 hello "+name
+print(hello)
+hello = f"4 hello {name}"
+print(hello)
+
+print("--------------------")
+print(name)
+name = "ran"
+print(name)
+print("--------------------")
+name2 = f'hello\n{name}'
+print(name2)
